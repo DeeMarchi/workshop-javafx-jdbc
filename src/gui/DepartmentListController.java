f2732e333180e5876244416afc081ed7e4cd483c
Denner Marchi <denner883@hotmail.com>
2019-11-25 20:10:03 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,6 +1,8 @@
 package gui;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,11 +11,34 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+
+    private DepartmentService service;
+
+    private ObservableList<Department> observableList;
+
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
+    }
+
+    public void updateTableView() {
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+        List<Department> list = service.findAll();
+        observableList = FXCollections.observableArrayList(list);
+        departmentTableView.setItems(observableList);
+    }
+
+    private void initializeNode() {
+
+    }
 
     @FXML
     private TableView<Department> departmentTableView;
@@ -32,9 +57,7 @@ public class DepartmentListController implements Initializable {
         System.out.println("onButtonNewAction");
     }
 
-    private void initializeNode() {
 
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
