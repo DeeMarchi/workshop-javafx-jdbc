f8b270954657f1fea3ce9937542049f7ab20b7a8
Denner Marchi <denner883@hotmail.com>
2019-11-27 19:33:11 -0300

    Adding database access

---
+++ src/model/dao/implementation/DepartmentDaoJDBC.java
@@ -0,0 +1,148 @@
+package model.dao.implementation;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    private Department instantiateDepartment(ResultSet set) throws SQLException {
+        Department department = new Department();
+        department.setId(set.getInt("Id"));
+        department.setName(set.getString("Name"));
+        return department;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "INSERT INTO department "
+                            + "(Name) "
+                            + "VALUES "
+                            + "(?)",
+                    statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, obj.getName());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet set = statement.getGeneratedKeys();
+                if (set.next()) {
+                    int id = set.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(set);
+            } else {
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public void update(Department obj) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "UPDATE department "
+                            + "SET name = ? "
+                            + "WHERE id = ?"
+            );
+            statement.setString(1, obj.getName());
+            statement.setInt(2, obj.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "DELETE FROM department where id = ?"
+            );
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement statement = null;
+        ResultSet set = null;
+
+        try {
+            statement = conn.prepareStatement("SELECT * FROM department WHERE department.Id = ?");
+            statement.setInt(1, id);
+            set = statement.executeQuery();
+
+            if (set.next()) {
+                return instantiateDepartment(set);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
+    }
+
+    @Override
+    public List<Department> findAll() {
+        PreparedStatement statement = null;
+        ResultSet set= null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "SELECT * FROM department "
+                        + "ORDER BY Name"
+            );
+            set = statement.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+            while (set.next()) {
+                list.add(instantiateDepartment(set));
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
+    }
+}
---
+++ src/model/dao/implementation/SellerDaoJDBC.java
@@ -0,0 +1,220 @@
+package model.dao.implementation;
+
+import db.DB;
+import db.DbException;
+import db.DbIntegrityException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    private Department instantiateDepartment(ResultSet set) throws SQLException {
+        Department department = new Department();
+        department.setId(set.getInt("DepartmentId"));
+        department.setName(set.getString("DepName"));
+        return department;
+    }
+
+    private Seller instantiateSeller(ResultSet set, Department department) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(set.getInt("Id"));
+        obj.setName(set.getString("Name"));
+        obj.setEmail(set.getString("Email"));
+        obj.setBaseSalary(set.getDouble("BaseSalary"));
+        obj.setBirthDate(set.getDate("BirthDate"));
+        obj.setDepartment(department);
+        return obj;
+    }
+
+    @Override
+    public void insert(Seller obj) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, obj.getName());
+            statement.setString(2, obj.getEmail());
+            statement.setDate(3, new Date(obj.getBirthDate().getTime()));
+            statement.setDouble(4, obj.getBaseSalary());
+            statement.setInt(5, obj.getDepartment().getId());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet set = statement.getGeneratedKeys();
+                if (set.next()) {
+                    int id = set.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(set);
+            } else {
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public void update(Seller obj) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE Id = ?"
+            );
+            statement.setString(1, obj.getName());
+            statement.setString(2, obj.getEmail());
+            statement.setDate(3, new Date(obj.getBirthDate().getTime()));
+            statement.setDouble(4, obj.getBaseSalary());
+            statement.setInt(5, obj.getDepartment().getId());
+            statement.setInt(6, obj.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+        } catch (SQLException | DbIntegrityException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+        }
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        PreparedStatement statement = null;
+        ResultSet set = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?"
+            );
+            statement.setInt(1, id);
+            set = statement.executeQuery();
+            if (set.next()) {
+                Department department = instantiateDepartment(set);
+                return instantiateSeller(set, department);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        PreparedStatement statement = null;
+        ResultSet set = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = Department.Id "
+                            + "ORDER BY Name"
+            );
+            set = statement.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (set.next()) {
+                Department dep = map.get(set.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(set);
+                    map.put(set.getInt("DepartmentId"), dep);
+                }
+                Seller obj = instantiateSeller(set, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement statement = null;
+        ResultSet set = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = Department.Id "
+                            + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name"
+            );
+            statement.setInt(1, department.getId());
+            set = statement.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (set.next()) {
+                Department dep = map.get(set.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(set);
+                    map.put(set.getInt("DepartmentId"), dep);
+                }
+                Seller obj = instantiateSeller(set, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(set);
+        }
+    }
+}
